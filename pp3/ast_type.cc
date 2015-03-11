9034aee0af1c5ee5e4c3cb2b80fb82a680e17e4b
Mike Shen <mikeshen@umich.edu>
2015-03-10 22:35:30 -0400

    Type Change

--- pp3/ast_type.cc
+++ pp3/ast_type.cc
@@ -22,12 +22,39 @@ Type *Type::nullType   = new Type("null");
 Type *Type::stringType = new Type("string");
 Type *Type::errorType  = new Type("error"); 
 
+extern SymTable *globalEnv;
+
 Type::Type(const char *n) {
     Assert(n);
     typeName = strdup(n);
 }
 
+bool NamedType::IsConvertableTo(Type *theType) {
+  Symbol *sym = globalEnv->find(id->getName(), CLASS);
+  ClassDecl *classDecl = static_cast<ClassDecl*>(sym->getNode());
+  char *newName = theType->getName();
 
+  if (theType->IsBuiltin())
+    return false;
+
+  if (IsEquivalentTo(theType))
+    return true;
+
+  if (sym  == nullptr)
+    return false;
+
+  if (classDecl->ImplementsInterface(newName))
+    return true;
+
+  return sym->getEnv()->subclassOf(newName);
+}
+
+bool NamedType::IsEquivalentTo(Type *theType) {
+  NamedType *ntheType = dynamic_cast<NamedType*>(theType);
+  if (ntheType == 0)
+    return false;
+  return strcmp(id->name(), ntheType->getName()) == 0;
+}
 
 	
 NamedType::NamedType(Identifier *i) : Type(*i->GetLocation()) {
@@ -41,4 +68,22 @@ ArrayType::ArrayType(yyltype loc, Type *et) : Type(loc) {
     (elemType=et)->SetParent(this);
 }
 
+bool ArrayType::IsConvertableTo(Type *theType) {
+  if (theType->IsEquivalentTo(Type::errorType))
+    return true;
+
+  ArrayType *ntheType = dynamic_cast<ArrayType*>(theType);
+  if (ntheType == 0)
+    return false;
+
+  return elemType->IsEquivalentTo(ntheType->getElemType());
+}
+
+bool ArrayType::IsEquivalentTo(Type *theType) {
+  ArrayType *ntheType = dynamic_cast<ArrayType*>(theType);
+  if (ntheType == 0) {
+    return false;
+  }
+  return elemType->IsEquivalentTo(ntheType->getElemType());
+}
 
